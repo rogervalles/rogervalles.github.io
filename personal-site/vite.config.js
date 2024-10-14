19546ab863b56aa13a9fad1167aed88e9e638b19
Rox <roger.valles@gmail.com>
2024-10-14 11:02:09 +0200

    updating base project in vite and package.json

--- personal-site/vite.config.js
+++ personal-site/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/rogervalles/rogervalles.github.io",
+  base: "/personal-site/",
   plugins: [react()],
 })
